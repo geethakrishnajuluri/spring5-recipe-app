424aaea399f3202efc3e67b4c1e013a3a117ac39
juluri.krishna <juluri.krishna@ivyvomptech.com>
2024-10-02 10:44:45 +0530

    Added project lombok and used in the models

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class IndexController {
 
     private final RecipeService recipeService;
@@ -16,7 +18,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
-
+        log.debug("Index page called!!");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
